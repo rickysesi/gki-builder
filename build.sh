cef7f0073de54204ead49f637b3f6c84755869d4
TegarXLu <botdimas67@gmail.com>
2026-02-02 06:59:57 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,9 +16,9 @@ TIMEZONE="Asia/Jakarta"
 ANYKERNEL_REPO="https://github.com/linastorvaldz/anykernel"
 KERNEL_DEFCONFIG="gki_defconfig"
 if [ "$KVER" == "6.6" ]; then
-  KERNEL_REPO="https://github.com/linastorvaldz/kernel-android15-6.6"
-  ANYKERNEL_BRANCH="android15-6.6"
-  KERNEL_BRANCH="android15-6.6-2025-01"
+  KERNEL_REPO="https://github.com/TegarXLu/onyx_clo"
+  ANYKERNEL_BRANCH="6.6"
+  KERNEL_BRANCH="6.6.77-capybara-clo-2.3"
 elif [ "$KVER" == "6.1" ]; then
   KERNEL_REPO="https://github.com/ramabondanp/android_kernel_common-6.1"
   ANYKERNEL_BRANCH="android14-6.1"
