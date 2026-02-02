a987657e654bd8a8062d5bfcafaebe637645c588
TegarXLu <botdimas67@gmail.com>
2026-02-02 07:42:01 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -314,6 +314,12 @@ if [ "$TODO" == "defconfig" ]; then
   upload_file $OUTDIR/.config
   exit 0
 fi
+# disable BBG
+echo ">> Disabling Baseband Guard"
+sed -i 's/^CONFIG_BBG=y/CONFIG_BBG=n/' out/.config || true
+cd ksrc
+make O=../out olddefconfig
+cd ..
 
 # Build the actual kernel
 log "Building kernel..."
