d56238f9a67733769415111e644a5afa65045b5c
TegarXLu <botdimas67@gmail.com>
2026-02-03 07:32:09 +0700

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -4,16 +4,6 @@
 #    Functions
 # ==============
 
-# send_msg
-send_msg() {
-  local MESSAGE="$1"
-  curl -s -X POST "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage" \
-    -d "chat_id=$TG_CHAT_ID" \
-    -d "disable_web_page_preview=true" \
-    -d "parse_mode=markdown" \
-    -d "text=$MESSAGE"
-}
-
 # KernelSU-related functions
 install_ksu() {
   local REPO="$1"
